4dddb4ab79148144de3240110c34f401a59f6d53
Lucas Diaz <Lucasdiazsilva99@gmail.com>
2021-12-23 01:18:17 -0300

    app.run

--- app.py
+++ app.py
@@ -2,10 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route('/')
+def index():
+    return 'Hola mundo'
 
 
 if __name__ == "__main__":
     app.run(debug=True)
+    app.run(port=80)
